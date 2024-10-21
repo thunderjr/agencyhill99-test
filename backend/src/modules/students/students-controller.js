d868f9ff1bee0cdf9fe6b69cb7838d2ba1b0f4b7
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2024-10-21 14:31:36 -0300

    feat (backend): implement students controller with project patterns

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -1,35 +1,47 @@
 const asyncHandler = require("express-async-handler");
-const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
+const {
+  getAllStudents,
+  addNewStudent,
+  getStudentDetail,
+  setStudentStatus,
+  updateStudent,
+} = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+  const students = await getAllStudents();
+  res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+  const payload = req.body;
+  const message = await addNewStudent(payload);
+  res.json(message);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id: studentId } = req.params;
+  const payload = req.body;
+  const message = await updateStudent({ ...payload, studentId });
+  res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id: studentId } = req.params;
+  const student = await getStudentDetail(studentId);
+  res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id: studentId } = req.params;
+  const payload = req.body;
+  const message = await setStudentStatus({ ...payload, studentId });
+  res.json(message);
 });
 
 module.exports = {
-    handleGetAllStudents,
-    handleGetStudentDetail,
-    handleAddStudent,
-    handleStudentStatus,
-    handleUpdateStudent,
+  handleGetAllStudents,
+  handleGetStudentDetail,
+  handleAddStudent,
+  handleStudentStatus,
+  handleUpdateStudent,
 };
